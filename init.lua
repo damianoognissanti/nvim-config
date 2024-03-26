35b023ec55beb3691639625511744a875f987f9c
Damiano Ognissanti <ognissanti@hotmail.se>
2024-03-26 09:58:56 +0100

    Added highlight to fold search + Cleanup
    - Added highlight to fold search
    - Changed toggle function so that it removes highlight when untoggling and reapplies it when retoggling
    - Cleanup of keybindings
    - Added a few comments

--- init.lua
+++ init.lua
@@ -1,21 +1,23 @@
 vim.cmd.colorscheme('gruvbox')
-vim.opt.ignorecase      = true
-vim.opt.number          = true
-vim.opt.cursorline      = true
-vim.opt.expandtab       = true
-vim.opt.clipboard       = 'unnamedplus'
-vim.opt.termguicolors   = true
-vim.opt.wildmode        = 'longest,list'
-vim.opt.tabstop         = 4
-vim.opt.softtabstop     = 4
-vim.opt.shiftwidth      = 4
-vim.opt.termguicolors   = true
-vim.opt.number          = true
-vim.g.mapleader         = ' '
-vim.g.VM_leader         = '<space>v'
-vim.o.timeout           = true
-vim.o.timeoutlen        = 200
-vim.opt.whichwrap       = vim.opt.whichwrap+'<,>,h,l,[,]'
+vim.opt.ignorecase         = true
+vim.opt.number             = true
+vim.opt.cursorline         = true
+vim.opt.expandtab          = true
+vim.opt.clipboard          = 'unnamedplus'
+vim.opt.termguicolors      = true
+vim.opt.wildmode           = 'longest,list'
+vim.opt.tabstop            = 4
+vim.opt.softtabstop        = 4
+vim.opt.shiftwidth         = 4
+vim.opt.termguicolors      = true
+vim.opt.number             = true
+vim.g.mapleader            = ' '
+vim.g.VM_leader            = '<space>v'
+vim.g.foldsearch_highlight = 1
+vim.o.timeout              = true
+vim.o.timeoutlen           = 200
+vim.opt.whichwrap          = vim.opt.whichwrap+'<,>,h,l,[,]'
+-- Open buffer at cursor location when last closed.
 vim.cmd([[ au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]])
 require('mini.surround').setup()
 require('fzf-lua').setup()
@@ -30,7 +32,7 @@ require('nvim-treesitter.configs').setup({
             node_decremental = 'n',
         },
     },
-    highlight  = { enable = true }, 
+    highlight  = { enable = true },
 })
 local wk = require('which-key')
 
@@ -42,48 +44,43 @@ wk.register({
         v = { name = 'Vim Visual Multi'},
     },
 })
-vim.keymap.set({'n','v'}, 'gl',               '$',                          {desc = 'Move to the end of the line' ,noremap=false})
-vim.keymap.set({'n','v'}, 'gj',               'G',                          {desc = 'Move to the end of the file' ,noremap=false})
-vim.keymap.set({'n','v'}, 'gk',               'gg',                         {desc = 'Move to the start of the file' ,noremap=false})
-vim.keymap.set({'n','v'}, 'gh',               '0',                          {desc = 'Move to the start of the line' ,noremap=false})
-vim.keymap.set({'n','v'}, 'U',                '<C-r>',                      {desc= 'Redo with U' })
-vim.keymap.set({'n','v'}, 'ö',                ':',                          {desc= 'Run command (:)', silent = false})
-vim.keymap.set({'n','v'}, 'ä',                '/',                          {desc= 'Search', silent = false})
-vim.keymap.set({'n','v'}, '<tab>',            '<cmd>bnext<cr>',             {desc= 'Switch to next buffer', silent = false})
-vim.keymap.set({'n','v'}, '<leader>pd',       '<cmd>FzfLua grep_cword<cr>', {desc = 'Word under cursor'  ,noremap=false})
-vim.keymap.set({'n','v'}, '<leader>ps',       '<cmd>FzfLua live_grep<cr>',  {desc = 'String you type'  ,noremap=false})
-vim.keymap.set({'n','v'}, '<leader>pf',       '<cmd>FzfLua files<cr>',      {desc = 'Filename'  ,noremap=false})
-vim.keymap.set({'n','v'}, '<leader>w',        '<cmd>w<cr>',                 {desc = 'Write file'  ,noremap=false})
-vim.keymap.set({'n','v'}, '<leader>q',        '<cmd>bd!<cr>',               {desc = 'Buffer Delete'  ,noremap=false})
-vim.keymap.set({'n','v'}, '<leader>g',        '<cmd>Gvdiffsplit<cr>',       {desc = 'Git diff split'  ,noremap=false})
-vim.keymap.set({'n','v'}, '<leader><leader>', '<cmd>nohlsearch<cr>',        {desc = 'Clear search highlight'  ,noremap=false})
-vim.keymap.set('n',       '<leader>r',        ':%s///g<Left><Left>',        {desc = 'Replace'  ,noremap=false, silent=false})
-vim.keymap.set('v',       '<leader>r',        ':s///g<Left><Left>',         {desc = 'Replace'  ,noremap=false, silent=false})
-
-vim.keymap.set('n', '<leader>s', [[':<C-u>/\<' . expand('<cword>') . '\><cr>:Fs<cr>']], { desc = 'Search highlight', noremap = true, silent = false, expr = true })
-
-vim.keymap.set('n', 'vv',       'V',                        {desc = 'Visual line'})
-vim.keymap.set('n', 'vc',       '<C-v>',                    {desc = 'Visual column'})
-vim.keymap.set('n', '<cr>',     ':call ToggleFold()<cr>zz', {noremap=true})
-vim.keymap.set('v', 'sa',       '<Nop>',                    {noremap=false})
-vim.keymap.set('v', 's',        ':VMSearch ',               {desc='Multicursor search', noremap=false,silent=false})
-
-vim.keymap.set('c', 'qq',       'q!',                                                                    {noremap=false})
-vim.keymap.set('c', '<leader>', [[getcmdtype() == "/" || getcmdtype() == "?" ? ".\\{-}" : "<space>"]],   {expr=true})
+vim.keymap.set({'n','v'}, 'gl',               '$',                          {desc = 'Move to the end of the line', noremap=false})
+vim.keymap.set({'n','v'}, 'gj',               'G',                          {desc = 'Move to the end of the file', noremap=false})
+vim.keymap.set({'n','v'}, 'gk',               'gg',                         {desc = 'Move to the start of the file', noremap=false})
+vim.keymap.set({'n','v'}, 'gh',               '0',                          {desc = 'Move to the start of the line', noremap=false})
+vim.keymap.set({'n','v'}, 'U',                '<C-r>',                      {desc = 'Redo with U' })
+vim.keymap.set({'n','v'}, 'ö',                ':',                          {desc = 'Run command (:)', silent = false})
+vim.keymap.set({'n','v'}, 'ä',                '/',                          {desc = 'Search', silent = false})
+vim.keymap.set('n',       'vv',               'V',                          {desc = 'Visual line'})
+vim.keymap.set({'n','v'}, '<tab>',            '<cmd>bnext<cr>',             {desc = 'Switch to next buffer', silent = false})
+vim.keymap.set({'n','v'}, '<leader>pd',       '<cmd>FzfLua grep_cword<cr>', {desc = 'Word under cursor'})
+vim.keymap.set({'n','v'}, '<leader>ps',       '<cmd>FzfLua live_grep<cr>',  {desc = 'String you type'})
+vim.keymap.set({'n','v'}, '<leader>pf',       '<cmd>FzfLua files<cr>',      {desc = 'Filename'})
+vim.keymap.set({'n','v'}, '<leader>w',        '<cmd>w<cr>',                 {desc = 'Write file'})
+vim.keymap.set({'n','v'}, '<leader>q',        '<cmd>bd!<cr>',               {desc = 'Buffer Delete'})
+vim.keymap.set({'n','v'}, '<leader>g',        '<cmd>Gvdiffsplit<cr>',       {desc = 'Git diff split'})
+vim.keymap.set({'n','v'}, '<leader><leader>', '<cmd>nohlsearch<cr>',        {desc = 'Clear search highlight'})
+vim.keymap.set('v',       'sa',               '<Nop>',                      {desc = 'No visual surround add: s is used for multicursors.'})
+vim.keymap.set('v',       's',                ':VMSearch ',                 {desc = 'Multicursor search', silent=false})
+vim.keymap.set('c',       'qq',               'q!',                         {desc = 'Rage quit'})
+vim.keymap.set('n',       '<cr>',             ':call ToggleFold()<cr>zz',   {desc = 'Toggle folds'})
+vim.keymap.set('n',       '<leader>ff',       ':call ToggleFold()<cr>zz',   {desc = 'Toggle folds'})
+-- Always fold seaches using Fold search.
+-- Space adds .\\{-} instead of space for fuzzy search.
 vim.keymap.set('c', '<cr>',     [[getcmdtype() == "/" || getcmdtype() == "?" ? "<cr>:Fs<cr>" : "<cr>"]], {expr=true})
-
+vim.keymap.set('c', '<space>', [[getcmdtype() == "/" || getcmdtype() == "?" ? ".\\{-}" : "<space>"]],   {expr=true})
 vim.cmd([[
 function! ToggleFold()
   let view = winsaveview()
   exec 'keepjumps normal! zj'
-  if foldclosed('.') > -1 
-      exec 'normal! zR' 
+  if foldclosed('.') > -1
+      exec 'Fe'
   else
       exec 'keepjumps normal! zk'
-      if foldclosed('.') > -1 
-          exec 'normal! zR' 
-      else 
-          exec 'normal! zM' 
+      if foldclosed('.') > -1
+          exec 'Fe'
+      else
+          exec 'Fl'
       endif
   endif
   call winrestview(view)
